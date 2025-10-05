f1e3ae816fadb734e51a0503eaf4d51865fbe1ef
ThiagoLemoos <thiago.lemos.ps@gmail.com>
2025-10-05 16:19:56 -0300

    feature: implementando Comparator

--- src/br/com/alura/screenmatch/principal/PrincipalComListas.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -7,6 +7,7 @@ import br.com.alura.screenmatch.modelos.Titulo;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 
 public class PrincipalComListas {
     public static void main(String[] args) {
@@ -41,5 +42,8 @@ public class PrincipalComListas {
 
         Collections.sort(lista);
         System.out.println(lista);
+
+        lista.sort(Comparator.comparing(Titulo::getAnoDeLancamento));
+        System.out.println("Ordenado:" + lista);
     }
 }
