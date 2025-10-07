738e857179c3f04412bfd8c682bf7da364d6ac96
ThiagoLemoos <thiago.lemos.ps@gmail.com>
2025-10-07 17:31:25 -0300

    feature: adicionado a lista de filmes a implementar com laço while, e escrita em um arquivo json

--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -7,46 +7,70 @@ import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
-        try {
-            Scanner leitura = new Scanner(System.in);
+
+        Scanner leitura = new Scanner(System.in);
+        List<Titulo> titulos = new ArrayList<>();
+        Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy
+                .UPPER_CAMEL_CASE)
+                .setPrettyPrinting()
+                .create();
+
+        var busca = "";
+        while(!busca.equalsIgnoreCase("sair")){
             System.out.println("Digite um filme para busca: ");
-            var busca = leitura.nextLine();
+            busca = leitura.nextLine();
+            if (busca.equalsIgnoreCase("sair")){
+                break;
+            }
             String endereco = "https://www.omdbapi.com/?t=" + busca.replace(" ", "+") + "&apikey=35e79e3f";
-            HttpClient client = HttpClient.newHttpClient();
+            try {
+                HttpClient client = HttpClient.newHttpClient();
 
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(endereco))
-                    .build();
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create(endereco))
+                        .build();
 
-            HttpResponse<String> response = client
-                    .send(request, HttpResponse.BodyHandlers.ofString());
+                HttpResponse<String> response = client
+                        .send(request, HttpResponse.BodyHandlers.ofString());
 
-            String json = response.body();
+                String json = response.body();
 
-            Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
-            TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
+                TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
 
 
-            Titulo meuTitulo = new Titulo(meuTituloOmdb);
-            System.out.println(meuTitulo);
-        } catch (NumberFormatException e){
-            System.out.println("Aconteceu um erro:");
-            System.out.println(e.getMessage());
-        } catch (IllegalArgumentException e){
-            System.out.println("Erro de Argumento inválido: ");
-            System.out.println(e.getMessage());
-        } catch (ErroDeConversaoDeAnoException e){
-            System.out.println(e.getMessage());
+                Titulo meuTitulo = new Titulo(meuTituloOmdb);
+                System.out.println(meuTitulo);
+                titulos.add(meuTitulo);
+
+
+            } catch (NumberFormatException e){
+                System.out.println("Aconteceu um erro:");
+                System.out.println(e.getMessage());
+            } catch (IllegalArgumentException e){
+                System.out.println("Erro de Argumento inválido: ");
+                System.out.println(e.getMessage());
+            } catch (ErroDeConversaoDeAnoException e){
+                System.out.println(e.getMessage());
+            }
+
         }
+        System.out.println(titulos);
 
+        FileWriter escrita = new FileWriter("filmes.json");
+        escrita.write(gson.toJson(titulos));
+        escrita.flush();
+        escrita.close();
     }
 }
